19d17e87ba01a58d8846eff8722a7292f141cc28
svlad4560 <svlad4560@gmail.com>
2020-10-19 21:27:54 -0700

    How to wright buttons with a message that pops up

--- GUI_for_Ana.py
+++ GUI_for_Ana.py
@@ -1,11 +1,13 @@
 from tkinter import *
 #what is TK
 root = Tk()
-# this is a label that will be created
-myLabel1 = Label(root, text = "sup").grid(row=0, column=0)
-myLabel2 = Label(root, text = "sup everybody").grid(row=1, column=0)
-# this puts label on the screan
-# myLabel1.grid(row=0, column=0)
-# myLabel2.grid(row=1, column=0)
-#this puts my screan up
+
+def myClick():
+    myLabel = Label(root, text = "your fucked")
+    myLabel.pack()
+
+
+myButton = Button(root, text = "click me", pady = 50, command = myClick)
+myButton.pack()
+
 root.mainloop()
